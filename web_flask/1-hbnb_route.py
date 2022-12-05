b3136108c542b7e453ffb8342ee6e6631555f376
alejandrodgz <alejandrodgz@github.com>
2022-12-05 12:55:18 -0500

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,9 +13,11 @@ def hello():
     '''just a friendly hello'''
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
