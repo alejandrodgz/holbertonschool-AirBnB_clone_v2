7590cbe923a345691cacabf121ea48acfd8be47d
alejandrodgz <alejandrodgz@github.com>
2022-12-05 13:46:23 -0500

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -35,8 +35,9 @@ def text(text):
     new_string = text.replace("_", " ")
     return "C {}".format(new_string)
 
-@app.route('/python/<text>', strict_slashes=False)
-def ptext(text="is cool"):
+@app.route('/python/', defaults={'text': "is cool"})
+@app.route('/python/<text>')
+def ptext(text):
     """
     just another comment:
         new_string: new string
