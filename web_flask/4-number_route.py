d7f938dd6a14f0f64ce62c778c5b49dd72a4c90f
alejandrodgz <alejandrodgz@github.com>
2022-12-05 14:03:00 -0500

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:         display "Hello HBNB!"
+            /hbnb:     display "HBNB"
+            /c/<text>: display "C" + text (replace underscores with space)
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """just a friendly hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """just another comment"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """
+    just another comment:
+        new_string: new string
+    """
+    new_string = text.replace("_", " ")
+    return "C {}".format(new_string)
+
+
+@app.route('/python/', defaults={'text': "is cool"})
+@app.route('/python/<text>')
+def ptext(text):
+    """
+    just another comment:
+        new_string: new string
+    """
+    new_string = text.replace("_", " ")
+    return "Python {}".format(new_string)
+
+
+@app.route('/python/<int:n>')
+def text(n):
+    """
+    just another comment:
+        n: must be a number
+    """
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=5000)
