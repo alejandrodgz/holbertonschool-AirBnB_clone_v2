d4b513c3ae04b52a51ef519e830c07065da621f3
alejandrodgz <alejandrodgz@github.com>
2022-12-05 13:16:40 -0500

    task3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,28 +7,35 @@ script starts Flask web app
             /c/<text>: display "C" + text (replace underscores with space)
 """
 
+
 from flask import Flask
+
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_hbnb():
-    """display text"""
+@app.route('/', strict_slashes=False)
+def hello():
+    """just a friendly hello"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """display text"""
+    """just another comment"""
     return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_text(text):
-    """display custom text given"""
-    return "C {}".format(text.replace('_', ' '))
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """
+    just another comment:
+        new_string: new string
+    """
+    new_string = text.replace("_", " ")
+    return f"C {new_string}"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    
     app.run(host="0.0.0.0", port=5000)
