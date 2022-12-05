4c16a55cfb27a0df5d1dfdb63b7fa368082776d9
alejandrodgz <alejandrodgz@github.com>
2022-12-05 12:46:16 -0500

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     '''just a friendly hello'''
-    return "<p>Hello HBNB!</p>"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
