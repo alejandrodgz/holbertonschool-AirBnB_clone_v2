570ebe49caf3b5575f3899b20862b16c5c429289
alejandrodgz <alejandrodgz@github.com>
2022-12-05 13:41:49 -0500

    task3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:         display "Hello HBNB!"
+            /hbnb:     display "HBNB"
+            /c/<text>: display "C" + text (replace underscores with space)
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """just a friendly hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """just another comment"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """
+    just another comment:
+        new_string: new string
+    """
+    new_string = text.replace("_", " ")
+    return "C {}".format(new_string)
+
+@app.route('/python/<text>', strict_slashes=False)
+def ptext(text="is cool"):
+    """
+    just another comment:
+        new_string: new string
+    """
+    new_string = text.replace("_", " ")
+    return "C {}".format(new_string)
+
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=5000)
